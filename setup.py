987f70d4c1d1f5bb66ca64c11555caa1a0973f4a
zigai <ziga.ivansek@gmail.com>
2022-07-24 20:00:58 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -6,23 +6,23 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 REQUIREMENTS = (HERE / "requirements.txt").read_text()
 
-setup(name="dathas",
-      version="0.0.4",
-      description=" Enhance your Python dataclasses",
-      long_description=README,
-      long_description_content_type="text/markdown",
-      author="Ziga Ivansek",
-      author_email="ziga.ivansek@gmail.com",
-      url="https://github.com/zigai/dathas",
-      license="MIT",
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9",
-          "Programming Language :: Python :: 3.10",
-          "Programming Language :: Python :: 3 :: Only",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      package_dir={"": "dathas"},
-      py_modules=["dathas"],
-      install_requires=REQUIREMENTS)
+setup(
+    name="dathas",
+    version="0.0.5",
+    description=" Enhance your Python dataclasses",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Ziga Ivansek",
+    author_email="ziga.ivansek@gmail.com",
+    url="https://github.com/zigai/dathas",
+    python_requires=">=3.10",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+)
